dff062344c003c4e820ca8e4c0ad8d0981607a99
Tschaffrin <72516618+Tibscha@users.noreply.github.com>
2025-12-07 18:48:29 +0100

    add tmux

--- nvim/.config/nvim/lua/plugins/neotree.lua
+++ nvim/.config/nvim/lua/plugins/neotree.lua
@@ -1,13 +1,18 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "MunifTanjim/nui.nvim",
-    "nvim-tree/nvim-web-devicons", -- optional, but recommended
-  },
-  config = function()
-    vim.keymap.set('n', '<leader>n', ':Neotree toggle<CR>', { desc = "Toggle Neo-tree" })
-    vim.keymap.set('n', '<leader>th', 'Neotree toggle show_hidden=true<CR>')
-  end
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"MunifTanjim/nui.nvim",
+		"nvim-tree/nvim-web-devicons", -- optional, but recommended
+	},
+	filesystem = {
+		follow_current_file = { enabled = true },
+		hijack_netrw_behavior = "open_default",
+		use_libuv_file_watcher = true, -- <─ 🔥 live refresh bei Dateisystem-Änderungen
+	},
+	config = function()
+		vim.keymap.set("n", "<leader>n", ":Neotree toggle<CR>", { desc = "Toggle Neo-tree" })
+		vim.keymap.set("n", "<leader>th", "Neotree toggle show_hidden=true<CR>")
+	end,
 }
--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -6,19 +6,17 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.keymap.set('i', '<C-s>', '<Esc>:w<CR>', { silent = true })
--- In Visual Mode: Text in System-Clipboard kopieren
-vim.keymap.set('v', '<leader>y', '"+y', { desc = "Copy to system clipboard" })
-
--- In Normal Mode: Aktuelle Zeile in System-Clipboard kopieren
-vim.keymap.set('n', '<leader>Y', '"+yy', { desc = "Copy line to system clipboard" })
-
--- In Normal Mode: Alles in System-Clipboard kopieren
-vim.keymap.set('n', '<leader>gy', 'gg"+yG', { desc = "Copy entire buffer to system clipboard" })
+vim.keymap.set("i", "<C-s>", "<Esc>:w<CR>", { silent = true })
+vim.keymap.set("v", "<leader>y", '"+y', { desc = "Copy to system clipboard" })
+vim.keymap.set("n", "<leader>Y", '"+yy', { desc = "Copy line to system clipboard" })
+vim.keymap.set("n", "<leader>gy", 'gg"+yG', { desc = "Copy entire buffer to system clipboard" })
+vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "python",
-  callback = function()
-    vim.opt.colorcolumn = "80"
-  end,
+	pattern = "python",
+	callback = function()
+		vim.opt.colorcolumn = "80"
+	end,
 })
-
